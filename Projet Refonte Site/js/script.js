e3e903741d701985cd8a777f11e8ffbe664b2065
Maxmed7 <46046493+Maxmed7@users.noreply.github.com>
2018-12-21 13:02:00 +0100

    Add files via upload

---
+++ Projet Refonte Site/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $('.menu-toggle').click(function() {
+  
+      $('.site-nav').toggleClass('site-nav--open', 500);
+      $(this).toggleClass('open');
+  
+    })
+  });
